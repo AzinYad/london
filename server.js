20435f222ccb66a629c5162280f38090b4f0618e
AzinYad <103781572+AzinYad@users.noreply.github.com>
2023-01-07 20:26:19 +0000

    level 300

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const Harrow = require("./data/Harrow.json");
-const Heathrow = require("./data/Heathrow.json");
-const Straford = require("./data/Stratford.json");
+const harrowData = require("./data/Harrow.json");
+const heathrowData = require("./data/Heathrow.json");
+const strafordData = require("./data/Stratford.json");
 const PORT = process.env.PORT || 3003;
 
 app.get("/", (req, res) => {
@@ -14,24 +14,30 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/Straford", (req, res) => {
-  res.send(Straford);
+app.get("/:city", (req, res) => {
+  let cityName = req.params.city.toLocaleLowerCase();
+  if (cityName === "straford") {
+    res.send(strafordData);
+  } else if (cityName === "harrow") {
+    res.send(harrowData);
+  } else if (cityName === "heathrow") {
+    res.send(heathrowData);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
-app.get("/Straford/pharmacies", (req, res) => {
-  res.send(Straford.pharmacies);
-});
-
-app.get("/Straford/colleges", (req, res) => {
-  res.send(Straford.colleges);
-});
-
-app.get("/Straford/doctors", (req, res) => {
-  res.send(Straford.doctors);
-});
-
-app.get("/Straford/hospitals", (req, res) => {
-  res.send(Straford.hospitals);
+app.get("/:city/pharmacies", (req, res) => {
+  let cityName = req.params.city.toLocaleLowerCase();
+  if (cityName === "straford") {
+    res.send(strafordData.pharmacies);
+  } else if (cityName === "harrow") {
+    res.send(harrowData.pharmacies);
+  } else if (cityName === "heathrow") {
+    res.send(heathrowData.pharmacies);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 const listener = app.listen(PORT, () => {
