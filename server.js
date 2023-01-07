f62bf8e61819f0a29ae008c90ab234ff313db178
AzinYad <103781572+AzinYad@users.noreply.github.com>
2023-01-07 19:36:19 +0000

    level 200

--- server.js
+++ server.js
@@ -9,8 +9,29 @@ app.get("/", (req, res) => {
   res.send({
     "/pharmacies": "returns an array of pharmacies in a specific area",
     "/colleges": "returns an array of colleges in a specific area",
-    "/schools": "returns an array of schools in a specific area",
+    "/doctors": "returns an array of doctors in a specific area",
+    "/hospitals": "returns an array of hospitals in a specific area",
   });
+});
+
+app.get("/Straford", (req, res) => {
+  res.send(Straford);
+});
+
+app.get("/Straford/pharmacies", (req, res) => {
+  res.send(Straford.pharmacies);
+});
+
+app.get("/Straford/colleges", (req, res) => {
+  res.send(Straford.colleges);
+});
+
+app.get("/Straford/doctors", (req, res) => {
+  res.send(Straford.doctors);
+});
+
+app.get("/Straford/hospitals", (req, res) => {
+  res.send(Straford.hospitals);
 });
 
 const listener = app.listen(PORT, () => {
